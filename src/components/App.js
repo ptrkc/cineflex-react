7694655f74df7214c0268e25d217183a8b088c57
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-05-11 02:05:39 -0300

    Changes to routing to make passing props easier.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
+import { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 import Header from "./Header";
 import Movies from "./Movies";
 import Dates from "./Dates";
 import Seats from "./Seats";
 import Success from "./Success";
+import Footer from "./Footer";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Header />
             <Switch>
-                <Route path="/contato" exact={true} component={Movies} />
-                <Route path="/filme" exact={true} component={Dates} />
-                <Route path="/sessao" exact={true} component={Seats} />
-                <Route path="/sucesso" exact={true} component={Success} />
+                <Route path="/" exact>
+                    <Movies />
+                </Route>
+                <Route path="/filme">
+                    <Dates />
+                </Route>
+                <Route path="/sessao">
+                    <Seats />
+                </Route>
+                <Route path="/sucesso">
+                    <Success />
+                </Route>
             </Switch>
+            {/* <Footer/> */}
         </BrowserRouter>
     );
 }
